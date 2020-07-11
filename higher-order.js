56bc5d1cd07d21e249911bf00288f4a9a74c8956
keatonBraith <keeters33@live.com>
2020-07-10 19:37:50 -0600

    finished higher-order

--- higher-order.js
+++ higher-order.js
@@ -22,14 +22,11 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-const evenFinder = (arr) => {
-  let evenNums = arr.filter((element, index, array) => {
+
+let evenNumbers = mixedNumbers.filter(function(element, index, array){
     return element % 2 === 0
   })
-  return evenNums
-}
 
-evenNums(mixedNumbers[element])
 
 
 
@@ -52,7 +49,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(function(element, index, wholeArray){
+  return element *= 1.07
+});
 
 
 
@@ -71,7 +70,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce(function(acc, curr){
+  return acc + curr
+})
 
 
 
@@ -97,7 +98,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(function(element, index, array){
+  return element.CP > 200
+})
 
 
 
@@ -114,7 +117,9 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map(function(element, index, array){
+  return element.price * (1+ element.tax)
+})
 
 
 
